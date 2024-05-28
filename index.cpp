a7bb1e50c576fc3ce766725dcf68ee896081c2f1
PrasadManamperi <prasaa.dmanamperi@gmail.com>
2024-05-28 06:01:30 +0000

    created Bi-directional association relationship

--- index.cpp
+++ index.cpp
@@ -32,52 +32,102 @@ public:
 };
 
 
-// Bi-directional Association [multiplicity eka classes dekema 0..*]
-class Student;
-class Department;
-
-class Student {
-private:
-  string name_p;
-  Department *dep[2];
-
-public:
-  Student() {}
-  Student(string pname, Department *pdep1) {
-    name_p = pname;
-    dep[0] = pdep1;
-  }
-  void sName() { cout << name_p << endl; }
-};
-
-class Department {
-private:
-  string depName;
-  Student *std[2];
-
-public:
-  Department() {}
-  Department(string pdepName, Student *pstd1, Student *pstd2) {
-    depName = pdepName;
-    std[0] = pstd1;
-    std[1] = pstd2;
-  }
-  void dName() {}
-  void Display() {
-    cout << "Department Name : " << depName << endl;
-    cout << "Student Names:-" << endl;
-    for (int i = 0; i < 2; i++) {
-      std[i]->sName();
-    }
-    cout << endl;
-  }
-};
-
-
 
 // Uni-directional Association
 class Employee {
-    
+    private:
+      string empID;
+      string name;
+    public:
+      Employee(string pempID, string pname) {
+        empID = pempID;
+        name = pname;
+      }
+      void displayEmployee(){
+        cout << "Employee Name : " << name << endl;
+        cout << "Employee ID : " << empID << endl;
+      }
+      ~Employee(){
+        cout << "Deleting Employee " << empID << endl;
+      }
+};
+
+class Department {
+  private:
+    string deptID;
+    string depName;
+    Employee *emp[SIZE];
+
+  public:
+    Department(string pdeptID, string pdepName, Employee *emp1, Employee *emp2) {
+      deptID = pdeptID;
+      depName = pdepName;
+      emp[0] = emp1;
+      emp[1] = emp2;
+    }
+    void displayDepartment() {
+      cout << "Department Details : " << deptID << " - " << depName << endl;
+      cout << "**************" << endl;
+      cout << "Employee Details : " << endl;
+      // godak employee la inna nisa loop ekak through ganna one.
+      for (int i = 0; i < SIZE; i++) {
+        emp[i]->displayEmployee();
+        cout << endl;
+      }
+    }
+    ~Department() {
+      cout << "Department Deleted " << deptID << endl;
+    }
+};
+
+//Bi-directional Association
+class Order; // bi-directional ekedi class dekama kalin declare karannaone (Forward declaration)
+class Customer;
+
+class Order {
+  private:
+    string ordID;
+    Customer *cust;
+  public:
+    Order(string pordID, Customer *pcust) {
+      ordID = pordID;
+      cust = pcust;
+      cust->addOrders(this);
+    }
+    void displayOrder() {
+      cout << "Order ID : " << ordID << endl;
+    }
+    ~Order(){
+      cout << "Order Deleted " << ordID << endl;
+    }
+};
+
+class Customer {
+  private:
+    string custID;
+    string custName;
+    Order *ord[SIZE];
+    int noOfOrders = 0;
+  public:
+    Customer() {}
+    Customer(string pcustID, string pcustName) {
+      custID = pcustID;
+      custName = pcustName;
+    }
+    void addOrders(Order *pord) {
+      if ( noOfOrders < SIZE ) 
+        ord[noOfOrders] = pord;
+      noOfOrders++;
+    }
+    void displayCustomer() {
+      cout << "Customer ID : " << custID << " - " << custName << endl;
+      for ( int i = 0; i < SIZE; i++ ) {
+       ord[i]->displayOrder();
+      }
+    }
+    ~Customer() {
+      cout << "Customer Deleted " << custID << endl;
+    }
 };
 
 
@@ -89,27 +139,22 @@ int main() {
   Car *c1 = new Car(134, "TOYOTA", "Axio");
   c1->Display();
 
-  cout << endl;
+  cout << endl << "-----------------------------" << endl;
 
 
 
+  // Uni-direction Assiosiation
+  Employee *emp1 = new Employee("E001", "Athula");
+  Employee *emp2 = new Employee("E002", "Sannasgala");
 
-  // Assiosiation
+  Department *dep1 = new Department("D001", "HR", emp1, emp2);
+  dep1->displayDepartment();
 
-  /*  Assiosiation eke class dekatama multiplicity eka 0..* nam issella ewage
-   * objects hadaganna one naththan kelawenawa eg. Student *s1 = new Student();
-   * Ehema hadaganna nam aniwarenma defualt constructor ekak hadagena thiyenna
-   * one. Itapasse object walata values add karaddi depend wenna class eke object ekata data insert karagena inna       one naththan print wenne na (methananam Student) */
+  delete dep1; //delete the dinamic memory 
 
-  Student *s1 = new Student();
-  Student *s2 = new Student();
-  Department *dep1 = new Department();
-
-  s1 = new Student("Kamal", dep1);
-  s2 = new Student("Nimal", dep1);
-
-  dep1 = new Department("dep1", s1, s2);
-  dep1->Display();
+  
+   cout << endl << "-----------------------------" << endl;
+  //Bi-directional Assiosiation
 
   return 0;
 }
